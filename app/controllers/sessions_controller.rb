1505279c48667219a07f04d5e91f757d0949f584
britneywright <blw06g@gmail.com>
2015-07-17 19:02:23 -0400

    implement user login and logout :honeybee: :honeybee: :honeybee:

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user.try(:authenticate, params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome Back!"
+    else
+      redirect_to root_path, flash: {error: "Invalid login"}
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Successfully logged out!"
+  end
+
+end
