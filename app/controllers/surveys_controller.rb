8a8d021d0fa1e742ebb97dd7ad505a92eb0fd7e6
Britney Wright <blw06g@gmail.com>
2015-07-18 14:05:19 -0400

    user can create a survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,28 @@
 class SurveysController < ApplicationController
+
+  def show
+    @survey = Survey.find_by(id: params[:id])
+  end
+
+  def new
+    @survey = Survey.new
+    question = @survey.questions.build
+    4.times { question.choices.build }
+  end
+
+  def create
+    survey = Survey.new(survey_params)
+    survey.creator_id = session[:user_id]
+    if survey.save
+      redirect_to survey, notice: "Survey created successfully."
+    else
+      render new_survey_path
+    end
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:title, :completion_message, questions_attributes: [:id, :body, choices_attributes: [:id, :body]] )
+  end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,5 @@
 class Survey < ActiveRecord::Base
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :questions
+  accepts_nested_attributes_for :questions, reject_if: lambda {|attributes| attributes['body'].blank?}
 end
